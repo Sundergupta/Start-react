8a3b692502273b95a292f279bd964e22b73ca6e2
Sundergupta <sunder1234gupta@gmail.com>
2024-09-03 18:29:35 +0530

    Delete icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Heading from './Heading.jsx'
+import List from './Heading.jsx'
 
 function App() {
   return (
     <>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
     <h1>Sunder is here</h1>
     </>
   )
